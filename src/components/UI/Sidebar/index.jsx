071d7649bb3003377aac41f8e8dd49a7428a5d29
Jonathan Camilo Cantillo Guerrero <jonathan@elemento43.com>
2020-03-04 17:05:57 -0500

    feat: Show the current language logo to the sidebar

--- src/components/UI/Sidebar/index.jsx
+++ src/components/UI/Sidebar/index.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import menuConfig from "./menu.config";
 /* Antd */
 import { Layout, Menu, Avatar, Row } from "antd";
+/* Assets */
+import reactLogo from "../../../assets/react.svg";
 
 /* Styles */
 if (typeof window === "object") {
@@ -34,7 +36,7 @@ function UISidebar() {
       >
         <Avatar
           style={{ backgroundColor: "#1890ff" }}
-          size={collapsed ? 40 : 250}
+          size={collapsed ? 40 : 200}
           src="https://avatars0.githubusercontent.com/u/22524458?s=460&v=4"
         />
       </Row>
@@ -49,6 +51,31 @@ function UISidebar() {
             );
           })}
       </Menu>
+      <div className="current-tech">
+        <header className="current-tech-header">
+          <img
+            src={reactLogo}
+            className="current-tech-logo"
+            style={{ height: collapsed ? 50 : 100 }}
+            alt="Current tech"
+          />
+          {!collapsed && (
+            <React.Fragment>
+              <p>
+                Currently working as a <code>React</code> developer
+              </p>
+              <a
+                className="current-tech-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </React.Fragment>
+          )}
+        </header>
+      </div>
     </Sider>
   );
 }
